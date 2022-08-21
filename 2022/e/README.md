f1be503c5333f5910cdfc1ba19da58090ddbd6c1
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-08-21 03:51:25 -0400

    screenshots and round e

---
+++ 2022/e/README.md
@@ -0,0 +1,7 @@
+#Round E
+
+I placed 8211 out of ~10350 people with 6 points. Here is the link to my submissions. 
+
+https://codingcompetitions.withgoogle.com/kickstart/submissions/00000000008cb0f5/0000000000943d46
+
+I started participating in the competition half way through. I passed the samples and the first test case but the program took too long on the second test case. After reviewing the second test case inputs, I realized why it took too long. I was nesting too much loops with large iterations.
--- 2022/e/main.py
+++ 2022/e/main.py
@@ -1,26 +1,21 @@
 from time import sleep
 
 
-def bot():
+def bot(i):
     global game_Board
     global bot_Score
-    if game_Board[0] == 'white':
-        game_Board[0] = 'Bot'
-        if len(game_Board) == 1:
-            return 1
-        return 0
+    if len(game_Board) == 1:
+        return 1
 
     #print(game_Board)
 
-    for i in range(1, len(game_Board) - 1):
-        #print(f'{i}   {game_Board[i - 1]}   {game_Board[i]}   {game_Board[i + 1]}  should equal {game_Board}')
-        if game_Board[i - 1] == 'white' and game_Board[i] == 'white' and game_Board[i + 1] == 'white':
-            game_Board[i] = 'Bot'
-            #print(game_Board)
-
-            bot_Score+=1
-            return 0
-    if game_Board[len(game_Board) - 1] == 'white' and game_Board[len(game_Board) - 2] == 'white':
+    #print(f'{i}   {game_Board[i - 1]}   {game_Board[i]}   {game_Board[i + 1]}  should equal {game_Board}')
+    if game_Board[i - 1] == 'white' and game_Board[i] == 'white' and game_Board[i + 1] == 'white':
+        game_Board[i] = 'Bot'
+        #print(game_Board)
+        bot_Score+=1
+        return 0
+    elif game_Board[len(game_Board) - 1] == 'white' and game_Board[len(game_Board) - 2] == 'white':
         game_Board[len(game_Board) - 1] = 'Bot'
         bot_Score+=1
         return 0
@@ -28,23 +23,22 @@ def bot():
 
 
 
-def john():
+def john(i):
     global game_Board
     global john_Score
 
-    for i in range(1, len(game_Board) - 1):
-        if game_Board[i - 2] == 'white' and game_Board[i - 1] == 'white' and game_Board[i] == 'white' and game_Board[i + 1] == 'white':
-            game_Board[i] = 'John'
-            john_Score+=1
-            #print(game_Board)
-            return 0
+    if game_Board[i - 2] == 'white' and game_Board[i - 1] == 'white' and game_Board[i] == 'white' and game_Board[i + 1] == 'white':
+        game_Board[i] = 'John'
+        john_Score+=1
+        #print(game_Board)
+        return 0
 
-    for i in range(1, len(game_Board) - 1):
-        if game_Board[i - 1] == 'white' and game_Board[i] == 'white' and game_Board[i + 1] == 'white':
-            game_Board[i] = 'John'
-            john_Score+=1
-            #print(game_Board)
-            return 0
+    if game_Board[i - 1] == 'white' and game_Board[i] == 'white' and game_Board[i + 1] == 'white':
+        game_Board[i] = 'John'
+        john_Score+=1
+        #print(game_Board)
+        return 0
+
     if game_Board[len(game_Board) - 1] == 'white' and game_Board[len(game_Board) - 2] == 'white':
         game_Board[len(game_Board) - 1] = 'John'
         john_Score+=1
@@ -70,17 +64,19 @@ def main():
         cells = int(test_Cases[i + 1])
 
         game_Board = ['white'] * cells
+        game_Board[0] = 'Bot'
         #print(game_Board)
         for g in range(len(game_Board)):
             print(g, end='\r')
             #print(game_Board)
-            if bot() == 1:     
+            if john(g) == 1:
+                print(f'Case #{i + 1}: {bot_Score}')
+                break
+            if bot(g) == 1:     
                 print(f'Case #{i + 1}: {bot_Score}')
                 break
             #print(game_Board)
-            if john() == 1:
-                print(f'Case #{i + 1}: {bot_Score}')
-                break
+            
 
 if __name__ == "__main__":
     main()
